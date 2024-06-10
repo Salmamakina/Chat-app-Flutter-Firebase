cb0ed04c3c299b9ec645a7909b5d25831883ce40
salma <salma.makinaa2557@gmail.com>
2024-06-10 15:29:47 +0100

    sec commit

---
+++ lib/screens/login_home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class LoginHome extends StatelessWidget {
+  const LoginHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
